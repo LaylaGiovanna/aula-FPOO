fd21311343347e18d7b5d4b9509946f347c8bba4
LaylaGiovanna <109964242+LaylaGiovanna@users.noreply.github.com>
2022-08-17 09:31:34 -0300

    Add files via upload

---
+++ juroSimples.java
@@ -0,0 +1,31 @@
+package br.senai.sp.jandira;
+
+import java.util.Scanner;
+
+public class juroSimples {
+
+	public static void main(String[] args) {
+		
+		double juros, capital, taxa, tempo;
+		Scanner leitor;
+		
+		leitor = new Scanner(System.in);
+		
+		System.out.print("Qual é o capital? ");
+		capital = leitor.nextDouble();
+		
+		System.out.print("Qual é a taxa de juros? ");
+		taxa = leitor.nextDouble();
+		
+		System.out.print("Qual é o tempo? ");
+		tempo = leitor.nextDouble();
+		
+		juros = capital * taxa / 100 * tempo; //j = c . i . t
+		
+		System.out.println("O valor do juros é de: " + juros );
+		
+		System.out.printf("Ao final do período você terá R$%s" , juros + capital);
+		 
+	}
+
+}
